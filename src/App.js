f09c889fb6a18ced0c382ee7aec1962be56bb323
Sterly Deracy <deracys1@tcnj.edu>
2023-05-29 22:04:51 -0400

    Add title component to root

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Title from './Components/Title/Title';
 import SearchBar from './Components/SearchBar/SearchBar';
 import WeatherResults from './Components/WeatherResults/WeatherResults';
 import './App.css';
@@ -7,6 +8,7 @@ function App() {
     <div id='top' className={`dark app`} style={{
       background: `url(${process.env.PUBLIC_URL + '/background.jpg'}) no-repeat center center fixed`
     }}>
+      <Title />
       <SearchBar />
       <WeatherResults />
     </div>
