ead3df2fdc58eb5f2b52f3b8df1cb2ca5c566d1c
Sterly Deracy <deracys1@tcnj.edu>
2023-05-31 14:40:20 -0400

    Create header component for title and searchBar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import SearchBar from "../SearchBar/SearchBar";
+import Title from "../Title/Title";
+
+const Header = () => {
+  return (
+    <header>
+      <Title />
+      <SearchBar />
+    </header>
+  );
+}
+
+export default Header;
