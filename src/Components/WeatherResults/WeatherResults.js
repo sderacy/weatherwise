b3eb89a99e6fb322ba797f0eb3cbff91ea77522d
Sterly Deracy <deracys1@tcnj.edu>
2023-05-31 14:36:50 -0400

    Implement styles for weather results card

--- src/Components/WeatherResults/WeatherResults.js
+++ src/Components/WeatherResults/WeatherResults.js
@@ -1,10 +1,23 @@
+import WaterDropIcon from '@material-ui/icons/InvertColors';
 import './WeatherResults.css'
+
+let today = new Date().toLocaleDateString()
 
 const WeatherResults = () => {
 
   return (
     <div className='results-card'>
-      <p>Results</p>
+      <div id='location' className='row'>
+        <h4>Ewing</h4>
+        <h6>{today}</h6>
+      </div>
+      <div id='temp' className='row'>
+        <h3>13&deg; C</h3>
+        <span> Stormy </span>
+      </div>
+      <div id='humidity' className='row'>
+        <span><WaterDropIcon /> 86%</span>
+      </div>
     </div>
   )
 }
