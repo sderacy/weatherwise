50da1267c98cc03766a7de7c83e790f3a69d94f5
Sterly Deracy <deracys1@tcnj.edu>
2023-06-01 20:36:00 -0400

    Implement togge-able button

--- src/Components/WeatherResults/WeatherResults.js
+++ src/Components/WeatherResults/WeatherResults.js
@@ -1,24 +1,39 @@
+import React, { useState } from 'react';
 import { InvertColors as WaterDropIcon, ToggleOff, ToggleOn } from '@material-ui/icons';
 import './WeatherResults.css'
 
 const today = new Date().toLocaleDateString()
 
-const WeatherResults = () => (
-  <div className='results-card'>
-    <div id='location' className='row'>
-      <h4>Ewing</h4>
-      <h6>{today}</h6>
-    </div>
-    <div id='temp' className='row'>
-      <h3>75&deg; F</h3>
-      <span> Stormy </span>
-    </div>
-    <div id='humidity' className='row'>
-      <span><WaterDropIcon /> 86%</span>
-      <span><ToggleOff /> F/C</span>
-    </div>
-  </div>
-)
+const WeatherResults = () => {
+  const [toggle, setToggle] = useState();
 
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className='results-card'>
+      <div id='location' className='row'>
+        <h4>Ewing</h4>
+        <h6>{today}</h6>
+      </div>
+      <div id='temp' className='row'>
+        <h3>75&deg; F</h3>
+        <span> Stormy </span>
+      </div>
+      <div id='humidity' className='row'>
+        <span><WaterDropIcon /> 86%</span>
+        <button
+          type='button'
+          onClick={handleToggle}
+          aria-label='toggle F/C'
+        >
+          {toggle ? <ToggleOff /> : <ToggleOn />}
+          <span>F/C</span>
+        </button>
+      </div>
+    </div>
+  )
+}
 
 export default WeatherResults;
