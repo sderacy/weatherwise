fe5324f3f60b25bc9892ffb839aad11761e8b943
Sterly Deracy <deracys1@tcnj.edu>
2023-06-16 12:57:11 -0400

    Add header tests

---
+++ src/tests/Header.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Header from './Header'
+
+test('Header renders correctly and handles search input', () => {
+  // Create a mock function for the onSearchInput callback
+  const onSearchInput = jest.fn()
+
+  // Render the Header component
+  const { getByPlaceholderText } = render(
+    <Header onSearchInput={onSearchInput} />
+  )
+
+  // Assert that the Title component is rendered
+  expect(getByPlaceholderText('City (e.g. Ewing)')).toBeInTheDocument()
+
+  // Simulate user input in the SearchBar component
+  const searchInput = 'New York'
+  fireEvent.change(getByPlaceholderText('City (e.g. Ewing)'), {
+    target: { value: searchInput },
+  })
+
+  // Assert that the onSearchInput callback is called with the correct input
+  expect(onSearchInput).toHaveBeenCalledWith(searchInput)
+})
