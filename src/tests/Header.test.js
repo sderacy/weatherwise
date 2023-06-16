757fd130a87bb5ba17e02c68eeec0d0fccc82d81
Sterly Deracy <deracys1@tcnj.edu>
2023-06-16 13:12:57 -0400

    Put tests in describe block

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,24 +2,26 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import Header from './Header'
 
-it('renders header correctly and handles search input', () => {
-  // Create a mock function for the onSearchInput callback
-  const onSearchInput = jest.fn()
+describe('Header', () => {
+  it('renders header correctly and handles search input', () => {
+    // Create a mock function for the onSearchInput callback
+    const onSearchInput = jest.fn()
 
-  // Render the Header component
-  const { getByPlaceholderText } = render(
-    <Header onSearchInput={onSearchInput} />
-  )
+    // Render the Header component
+    const { getByPlaceholderText } = render(
+      <Header onSearchInput={onSearchInput} />
+    )
 
-  // Assert that the Title component is rendered
-  expect(getByPlaceholderText('City (e.g. Ewing)')).toBeInTheDocument()
+    // Assert that the Title component is rendered
+    expect(getByPlaceholderText('City (e.g. Ewing)')).toBeInTheDocument()
 
-  // Simulate user input in the SearchBar component
-  const searchInput = 'New York'
-  fireEvent.change(getByPlaceholderText('City (e.g. Ewing)'), {
-    target: { value: searchInput },
+    // Simulate user input in the SearchBar component
+    const searchInput = 'New York'
+    fireEvent.change(getByPlaceholderText('City (e.g. Ewing)'), {
+      target: { value: searchInput },
+    })
+
+    // Assert that the onSearchInput callback is called with the correct input
+    expect(onSearchInput).toHaveBeenCalledWith(searchInput)
   })
-
-  // Assert that the onSearchInput callback is called with the correct input
-  expect(onSearchInput).toHaveBeenCalledWith(searchInput)
 })
--- src/tests/Title.test.js
+++ src/tests/Title.test.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Title from './Title'
 
-it('renders the title correctly', () => {
-  // Render the Title component
-  const { getByTestId } = render(<Title />)
+describe('Title', () => {
+  it('renders the title correctly', () => {
+    // Render the Title component
+    const { getByTestId } = render(<Title />)
 
-  // Get the title element by its test id
-  const titleElement = getByTestId('title')
+    // Get the title element by its test id
+    const titleElement = getByTestId('title')
 
-  // Assert that the title text is correct
-  expect(titleElement.textContent).toBe('Weather Wise')
+    // Assert that the title text is correct
+    expect(titleElement.textContent).toBe('Weather Wise')
+  })
 })
